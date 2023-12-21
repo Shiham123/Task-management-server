30c6d0779583a72ea149322384d92658cb3176ab
Shiham123 <s36651786@gmail.com>
2023-12-21 14:58:56 +0600

    database connected

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const port = 3000;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster-assignment-11.m6efgmp.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = 'mongodb://localhost:27017';
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    // await client.connect();
+
+    await client.db('admin').command({ ping: 1 });
+    console.log('You successfully connected to MongoDB!');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+run();
+
+app.get('/', async (request, response) => {
+  response.send('successfully connected');
+});
+
+app.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}`);
+});
