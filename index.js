5ea09158878d8a56025e65d7162294c6aa36abf0
Shiham123 <s36651786@gmail.com>
2023-12-22 10:02:26 +0600

    deployed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster-assignment-11.m6efgmp.mongodb.net/?retryWrites=true&w=majority`;
